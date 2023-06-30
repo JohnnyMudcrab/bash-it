a9fb0a05c8855e54f6388e2b03fff2d503ace67c
Magnus <magnus.hanses@iff.fraunhofer.de>
2023-06-30 14:51:48 +0200

    add ros aliases

--- aliases/available/ros2.aliases.bash
+++ aliases/available/ros2.aliases.bash
@@ -1,4 +1,6 @@
 about-alias 'ros2 abbreviations'
 alias cb="colcon build --cmake-args -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_CXX_FLAGS=\"-isystem /opt/ros/humble/include\" --symlink-install"
+alias cbd="colcon build --cmake-args -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_CXX_FLAGS=\"-isystem /opt/ros/humble/include\" --symlink-install"
+alias cbrd="colcon build --cmake-args -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_CXX_FLAGS=\"-isystem /opt/ros/humble/include\" --symlink-install"
 alias rd="rosdep install --ignore-src --from-paths src -y -r"
 alias rc="vcs import"
